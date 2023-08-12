9bec971878c71a2539dfe162890b73305b6f2668
altair961 <altair89@list.ru>
2023-08-12 19:40:15 +0300

    Empty project was added. SFML was initialized.

---
+++ Shmup/shmup/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace shmup
+{
+    class Program
+    {
+        const int WIDTH = 640;
+        const int HEIGHT = 480;
+        const string TITLE = "SHMUP";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            VideoMode mode = new VideoMode(WIDTH, HEIGHT);
+            RenderWindow window = new RenderWindow(mode, TITLE);
+            window.SetVerticalSyncEnabled(true);
+            window.Closed += (sender, args) => window.Close();
+
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(Color.Blue);
+                window.Display();
+            }
+
+        }
+    }
+}
+
