e3605629fd52d06e7c7672eebd7707c43dc9d51c
altair961 <altair89@list.ru>
2023-08-12 20:11:24 +0300

    Switched to Visual Studio.

---
+++ Shmup/Shmup/Game.cs
@@ -0,0 +1,50 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace Shmup
+{
+    public class Game
+    {
+        private const int WIDTH = 640;
+        private const int HEIGHT = 480;
+        private const string TITLE = "SHMUP";
+        private RenderWindow window;
+        private VideoMode mode = new VideoMode(WIDTH, HEIGHT);
+
+        public Game()
+        {
+            this.window = new RenderWindow(this.mode, TITLE);
+
+            this.window.SetVerticalSyncEnabled(true);
+            this.window.Closed += (sender, args) => {
+                this.window.Close();
+            };
+        }
+
+        public void run()
+        {
+            while (this.window.IsOpen)
+            {
+                this.handleEvents();
+                this.update();
+                this.draw();
+            }
+        }
+
+        private void handleEvents()
+        {
+            this.window.DispatchEvents();
+        }
+
+        private void update()
+        {
+
+        }
+
+        private void draw()
+        {
+            this.window.Clear(Color.Blue);
+            this.window.Display();
+        }
+    }
+}
